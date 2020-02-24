1219be15dc63bf6381a16ceb54da3c1af6e4124b
Avinash Dhawan <avinash.dhawan@gmail.com>
2020-02-23 23:30:09 -0500

    seems to have all data working

--- PyBank/main.py
+++ PyBank/main.py
@@ -9,6 +9,7 @@ csvdata = os.path.join('..', 'Resources', 'budget_data.csv')
 
 MonthYear = []
 ProfitorLoss = []
+average_difference = []
 
 with open(csvdata) as csvfile:
 
@@ -28,19 +29,50 @@ with open(csvdata) as csvfile:
 
 Months = len(MonthYear)        
 Total = sum(ProfitorLoss)
-Average = Total / Months
-Maximum = max(ProfitorLoss)
-Minimum = min(ProfitorLoss)
-MaxIndex = max(ProfitorLoss).index
 
 
+#MaxIndex = max(ProfitorLoss).index
 
-print(Total)
-print(Months)
-print(round(Average))
-print(Maximum)
-print(Minimum)
+for i in range(len(ProfitorLoss)):
+    
+    previous = ProfitorLoss[i-1]
+
+    current = ProfitorLoss[i]
+        
+    difference = current - previous
+    average_difference.append(difference)
+
+
+Average = (sum(average_difference) - average_difference[0]) / (int(Months) -1)
+Maximum = max(average_difference)
+Minimum = min(average_difference)
+MaxIndex = average_difference.index(max(average_difference))
+MinIndex = average_difference.index(min(average_difference))
 print(MaxIndex)
+print(MinIndex)
+#MaxIndex + 1 because average difference ignores first cell as difference of 2 cells
+MaxDate = MonthYear[MaxIndex]
+MinDate = MonthYear[MinIndex]
+print(MaxDate)
+print(MinDate)
+
+Summary_File = (MonthYear, ProfitorLoss, average_difference)
+
+Output = os.path.join("Summarized_main.csv")
+
+with open(Output, "w") as datafile:
+    writer = csv.writer(datafile)
+
+    writer.writerows(Summary_File)
+
+
+
+print("Total Profit or Loss is ", Total)
+print("Number of Month's of Data is ", Months)
+print("The Average Difference is ", round(Average,2))
+print("Max is ", Maximum)
+print("Min is ", Minimum)
+#print(MaxIndex)
 
 
 
